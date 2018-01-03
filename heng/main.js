b49b38531410c5b0c9ee9bbbf73b9cce78f4103b
zechaozhang <zechao.zhang@langtaojin.com>
2018-01-03 19:01:10 +0800

    kk

--- heng/main.js
+++ heng/main.js
@@ -2,20 +2,25 @@
   var app = document.getElementById('app')
   var pagesWrap = document.querySelector('.pages')
   var pages = document.querySelectorAll('.page')
+  var cur = 0
 
   pagesWrap.style.cssText = 'height:'+ pages.length * 100 + '%'
   ;[].slice.call(pages).forEach(function (t) {
     t.style.cssText = 'height:' + 100/pages.length +'%'
   })
 
-  var varObj = {
+  var pagesWrapHeight = pagesWrap.clientHeight
+
+  var slideNum = {
     mouseStartX: 0,
     mouseStartY: 0,
     mouseEndX: 0,
     mouseEndY:0,
+    eleY:0,
     disX:0,
     disY:0,
-    isY:false,
+    isFirst:true,
+    isY:true
   }
 
   document.addEventListener('touchstart', function (e) {
@@ -25,27 +30,51 @@
   //手指按下
   app.addEventListener('touchstart', function (e) {
     var touches = e.changedTouches[0]
-    varObj.mouseStartX = touches.clientX
-    varObj.mouseStartY = touches.clientY
+    slideNum.mouseStartX = touches.clientX
+    slideNum.mouseStartY = touches.clientY
+    slideNum.eleY = pagesWrap.getBoundingClientRect().top
+    pagesWrap.style.transition = ''
+    slideNum.isY = true
+    slideNum.isFirst = true
   })
 
   //手指移动
   app.addEventListener('touchmove', function (e) {
     var touches = e.changedTouches[0]
-    varObj.mouseEndX = touches.clientX
-    varObj.mouseEndY = touches.clientY
-    varObj.disX =  varObj.mouseEndX - varObj.mouseStartX
-    varObj.disY = varObj.mouseEndY - varObj.mouseStartY
-    if(Math.abs(varObj.disX) > Math.abs(varObj.disY)) {
-      console.log(123);
-      varObj.isY = false
-      return
+    slideNum.mouseEndX = touches.clientX
+    slideNum.mouseEndY = touches.clientY
+    slideNum.disX =  slideNum.mouseEndX - slideNum.mouseStartX
+    slideNum.disY = slideNum.mouseEndY - slideNum.mouseStartY
+    if(!slideNum.isY) return
+    if(slideNum.isFirst) {
+      slideNum.isFirst = false
+      if(Math.abs(slideNum.disY) < Math.abs(slideNum.disX)) {
+        slideNum.isY = false
+        return
+      }
     }
-    pagesWrap.style.transform = 'translateY('+ varObj.disY +'px)'
+    var t = 0
+    if(slideNum.disY > 0) {
+      t = (1 - ((pagesWrapHeight - slideNum.disY)/pagesWrapHeight))*slideNum.disY
+    }else {
+      t = (1 - (pagesWrapHeight + slideNum.disY)/pagesWrapHeight)*slideNum.disY
+    }
+    var totalTranslate = slideNum.eleY + slideNum.disY
+    pagesWrap.style.transform = 'translateY('+ totalTranslate +'px)'
   })
 
   //手指按下
   app.addEventListener('touchend', function (e) {
-
+    if(Math.abs(slideNum.disY) > 200) {
+      slideNum.disY < 0 ? cur++ : cur--
+    }
+    if(cur < 0) {
+      cur = 0
+    }
+    if(cur === pages.length) {
+      cur = pages.length - 1
+    }
+    pagesWrap.style.transform = 'translateY('+ -cur*(100/pages.length) +'%)'
+    pagesWrap.style.transition = 'transform .5s'
   })
 })()
